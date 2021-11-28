ed433767a5984907690200a3b46e4a29d2a4ab60
Michael Allen <mallenjr42@gmail.com>
2021-11-28 11:41:19 -0600

    updated train script

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import tensorflow as tf
+from tensorflow._api.v2 import audio
 
 from tensorflow.keras import layers
 from tensorflow.keras import models
@@ -15,3 +16,99 @@ seed = 42
 tf.random.set_seed(seed)
 np.random.seed(seed)
 
+DATASET_PATH = './data'
+
+data_dir = pathlib.Path(DATASET_PATH)
+
+def load_single_audio_file():
+
+  f = tf.io.read_file('./data/bed/1aed7c6d_nohash_0.wav')
+
+  audio, _ = tf.audio.decode_wav(contents=f)
+  print(audio.shape)
+
+  parts = tf.strings.split(
+      input='./data/bed/1aed7c6d_nohash_0.wav',
+      sep=os.path.sep)
+
+  print(parts[-2])
+
+def get_label(file_path):
+  parts = tf.strings.split(
+      input=file_path,
+      sep=os.path.sep)
+  # Note: You'll use indexing here instead of tuple unpacking to enable this
+  # to work in a TensorFlow graph.
+  return parts[-2]
+
+def decode_audio(audio_binary):
+  # Decode WAV-encoded audio files to `float32` tensors, normalized
+  # to the [-1.0, 1.0] range. Return `float32` audio and a sample rate.
+  audio, _ = tf.audio.decode_wav(contents=audio_binary)
+  # Since all the data is single channel (mono), drop the `channels`
+  # axis from the array.
+  return tf.squeeze(audio, axis=-1)
+
+
+
+def get_waveform_and_label(file_path):
+  label = get_label(file_path)
+  audio_binary = tf.io.read_file(file_path)
+  waveform = decode_audio(audio_binary)
+  return waveform, label
+
+def plot_audio_files():
+
+  commands = np.array(tf.io.gfile.listdir(str(data_dir)))
+  commands = commands[commands != 'README.md']
+  print('Commands:', commands)
+
+
+
+  filenames = tf.io.gfile.glob(str(data_dir) + '/*/*')
+  filenames = tf.random.shuffle(filenames)
+  num_samples = len(filenames)
+  print('Number of total examples:', num_samples)
+  print('Number of examples per label:',
+        len(tf.io.gfile.listdir(str(data_dir/commands[0]))))
+  print('Example file tensor:', filenames[0])
+
+  train_files = filenames[:6400]
+  val_files = filenames[6400: 6400 + 800]
+  test_files = filenames[-800:]
+
+  print('Training set size', len(train_files))
+  print('Validation set size', len(val_files))
+  print('Test set size', len(test_files))
+
+
+  AUTOTUNE = tf.data.AUTOTUNE
+
+  files_ds = tf.data.Dataset.from_tensor_slices(train_files)
+
+  waveform_ds = files_ds.map(
+      map_func=get_waveform_and_label,
+      num_parallel_calls=AUTOTUNE)
+
+
+  rows = 3
+  cols = 3
+  n = rows * cols
+  fig, axes = plt.subplots(rows, cols, figsize=(10, 12))
+
+  for i, (audio, label) in enumerate(waveform_ds.take(n)):
+    r = i // cols
+    c = i % cols
+    ax = axes[r][c]
+    ax.plot(audio.numpy())
+    ax.set_yticks(np.arange(-1.2, 1.2, 0.2))
+    label = label.numpy().decode('utf-8')
+    ax.set_title(label)
+
+  plt.show()
+
+
+
+
+if __name__ == "__main__":
+  plot_audio_files()
