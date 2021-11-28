f4f33005224dd79a61ca85257bf4213ceea1856c
Michael Allen <mallenjr42@gmail.com>
2021-11-28 12:22:04 -0600

    update train.py

--- train.py
+++ train.py
@@ -57,6 +57,29 @@ def get_waveform_and_label(file_path):
   waveform = decode_audio(audio_binary)
   return waveform, label
 
+def get_spectrogram(waveform):
+  # Zero-padding for an audio waveform with less than 16,000 samples.
+  input_len = 16000
+  waveform = waveform[:input_len]
+  zero_padding = tf.zeros(
+      [16000] - tf.shape(waveform),
+      dtype=tf.float32)
+  # Cast the waveform tensors' dtype to float32.
+  waveform = tf.cast(waveform, dtype=tf.float32)
+  # Concatenate the waveform with `zero_padding`, which ensures all audio
+  # clips are of the same length.
+  equal_length = tf.concat([waveform, zero_padding], 0)
+  # Convert the waveform to a spectrogram via a STFT.
+  spectrogram = tf.signal.stft(
+      equal_length, frame_length=255, frame_step=128)
+  # Obtain the magnitude of the STFT.
+  spectrogram = tf.abs(spectrogram)
+  # Add a `channels` dimension, so that the spectrogram can be used
+  # as image-like input data with convolution layers (which expect
+  # shape (`batch_size`, `height`, `width`, `channels`).
+  spectrogram = spectrogram[..., tf.newaxis]
+  return spectrogram
+
 def plot_audio_files():
 
   commands = np.array(tf.io.gfile.listdir(str(data_dir)))
@@ -82,7 +105,10 @@ def plot_audio_files():
   print('Test set size', len(test_files))
 
 
-  AUTOTUNE = tf.data.AUTOTUNE
+  try:
+    AUTOTUNE = tf.data.AUTOTUNE     
+  except:
+    AUTOTUNE = tf.data.experimental.AUTOTUNE 
 
   files_ds = tf.data.Dataset.from_tensor_slices(train_files)
 
