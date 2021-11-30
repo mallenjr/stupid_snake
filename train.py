ec329968d9f3ac16077bb37f90961619f58aa699
Michael Allen <mallenjr42@gmail.com>
2021-11-30 08:05:23 -0600

    demo complete on train.py

--- train.py
+++ train.py
@@ -20,6 +20,18 @@ DATASET_PATH = './data'
 
 data_dir = pathlib.Path(DATASET_PATH)
 
+gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+for device in gpu_devices:
+    tf.config.experimental.set_memory_growth(device, True)
+
+commands = np.array(tf.io.gfile.listdir(str(data_dir)))
+commands = commands[commands != 'README.md']
+
+try:
+  AUTOTUNE = tf.data.AUTOTUNE     
+except:
+  AUTOTUNE = tf.data.experimental.AUTOTUNE 
+
 def load_single_audio_file():
 
   f = tf.io.read_file('./data/bed/1aed7c6d_nohash_0.wav')
@@ -94,17 +106,32 @@ def plot_spectrogram(spectrogram, ax):
   Y = range(height)
   ax.pcolormesh(X, Y, log_spec)
 
+def get_spectrogram_and_label_id(audio, label):
+  spectrogram = get_spectrogram(audio)
+  label_id = tf.argmax(tf.cast(label == commands, tf.float32))
+  return spectrogram, label_id
+
+def preprocess_dataset(files):
+  files_ds = tf.data.Dataset.from_tensor_slices(files)
+  output_ds = files_ds.map(
+      map_func=get_waveform_and_label,
+      num_parallel_calls=AUTOTUNE)
+  output_ds = output_ds.map(
+      map_func=get_spectrogram_and_label_id,
+      num_parallel_calls=AUTOTUNE)
+  return output_ds
 
 def plot_audio_files():
-
-  commands = np.array(tf.io.gfile.listdir(str(data_dir)))
-  commands = commands[commands != 'README.md']
   print('Commands:', commands)
-
-
 
   filenames = tf.io.gfile.glob(str(data_dir) + '/*/*')
   filenames = tf.random.shuffle(filenames)
+
+  train_files = filenames[:6400]
+  val_files = filenames[6400: 6400 + 800]
+  test_files = filenames[-800:]
+
+
   num_samples = len(filenames)
   print('Number of total examples:', num_samples)
   print('Number of examples per label:',
@@ -119,52 +146,99 @@ def plot_audio_files():
   print('Validation set size', len(val_files))
   print('Test set size', len(test_files))
 
-
-  try:
-    AUTOTUNE = tf.data.AUTOTUNE     
-  except:
-    AUTOTUNE = tf.data.experimental.AUTOTUNE 
-
   files_ds = tf.data.Dataset.from_tensor_slices(train_files)
 
   waveform_ds = files_ds.map(
       map_func=get_waveform_and_label,
       num_parallel_calls=AUTOTUNE)
 
+  spectrogram_ds = waveform_ds.map(
+    map_func=get_spectrogram_and_label_id,
+    num_parallel_calls=AUTOTUNE)
 
-  rows = 3
-  cols = 3
-  n = rows * cols
-  fig, axes = plt.subplots(rows, cols, figsize=(10, 12))
+  train_ds = spectrogram_ds
+  val_ds = preprocess_dataset(val_files)
+  test_ds = preprocess_dataset(test_files)
 
-  # for i, (audio, label) in enumerate(waveform_ds.take(n)):
-  #   r = i // cols
-  #   c = i % cols
-  #   ax = axes[r][c]
-  #   ax.plot(audio.numpy())
-  #   ax.set_yticks(np.arange(-1.2, 1.2, 0.2))
-  #   label = label.numpy().decode('utf-8')
-  #   ax.set_title(label)
+  batch_size = 64
+  train_ds = train_ds.batch(batch_size)
+  val_ds = val_ds.batch(batch_size)
 
-  for waveform, label in waveform_ds.take(1):
-    label = label.numpy().decode('utf-8')
-    spectrogram = get_spectrogram(waveform)
+  train_ds = train_ds.cache().prefetch(AUTOTUNE)
+  val_ds = val_ds.cache().prefetch(AUTOTUNE)
 
-  print('Label:', label)
-  print('Waveform shape:', waveform.shape)
-  # print('Spectrogram shape:', spectrogram.shape)
-  # print('Audio playback')
-  # display.display(display.Audio(waveform, rate=16000))
+  for spectrogram, _ in spectrogram_ds.take(1):
+    input_shape = spectrogram.shape
+  print('Input shape:', input_shape)
+  num_labels = len(commands)
 
-  # fig, axes = plt.subplots(2, figsize=(12, 8))
-  # timescale = np.arange(waveform.shape[0])
-  # axes[0].plot(timescale, waveform.numpy())
-  # axes[0].set_title('Waveform')
-  # axes[0].set_xlim([0, 16000])
+  print(tf.__version__)
 
-  # plot_spectrogram(spectrogram.numpy(), axes[1])
-  # axes[1].set_title('Spectrogram')
-  # plt.show()
+  # Instantiate the `tf.keras.layers.Normalization` layer.
+  norm_layer = tf.keras.layers.experimental.preprocessing.Normalization(
+    axis=None)
+  # Fit the state of the layer to the spectrograms
+  # with `Normalization.adapt`.
+  norm_layer.adapt(data=spectrogram_ds.map(map_func=lambda spec, label: spec))
+
+  model = models.Sequential([
+      layers.Input(shape=input_shape),
+      # Downsample the input.
+      layers.Resizing(32, 32),
+      # Normalize.
+      norm_layer,
+      layers.Conv2D(32, 3, activation='relu'),
+      layers.Conv2D(64, 3, activation='relu'),
+      layers.MaxPooling2D(),
+      layers.Dropout(0.25),
+      layers.Flatten(),
+      layers.Dense(128, activation='relu'),
+      layers.Dropout(0.5),
+      layers.Dense(num_labels),
+  ])
+
+  model.summary()
+
+  model.compile(
+    optimizer=tf.keras.optimizers.Adam(),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'],
+  )
+
+  EPOCHS = 15
+  history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=EPOCHS,
+    callbacks=tf.keras.callbacks.EarlyStopping(verbose=1, patience=2),
+  )
+
+  metrics = history.history
+  plt.plot(history.epoch, metrics['loss'], metrics['val_loss'])
+  plt.legend(['loss', 'val_loss'])
+  plt.show()
+
+  test_audio = []
+  test_labels = []
+
+  for audio, label in test_ds:
+    test_audio.append(audio.numpy())
+    test_labels.append(label.numpy())
+
+  test_audio = np.array(test_audio)
+  test_labels = np.array(test_labels)
+
+  y_pred = np.argmax(model.predict(test_audio), axis=1)
+  y_true = test_labels
+
+  test_acc = sum(y_pred == y_true) / len(y_true)
+  print(f'Test set accuracy: {test_acc:.0%}')
+
+
+
+
+
+
 
 
 
