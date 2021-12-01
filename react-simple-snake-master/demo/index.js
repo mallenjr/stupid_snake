b0e6bbd2b27759eef24d3c27bd273d958411cea5
Michael Allen <mallenjr42@gmail.com>
2021-12-01 11:38:26 -0600

    updated application styling

--- react-simple-snake-master/demo/index.js
+++ react-simple-snake-master/demo/index.js
@@ -5,18 +5,8 @@ import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>react-simple-snake</h1>
-    <div className="textWrapper">
-      <p className="subTitle">
-      <a href="https://www.npmjs.com/package/react-simple-snake" target="_blank" rel="noopener noreferrer">NPM</a> -
-      <a href="https://github.com/MaelDrapier/react-simple-snake" target="_blank" rel="noopener noreferrer"> Github</a>
-      </p>
-      <p id="instructions">Use the arrow keys or W/A/S/D to play</p>
-      <p className="subTitle">A simple snake game created with the
-      <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer"> React </a>
-      JavaScript library</p>
-    </div>
+    <div id='spacer' />
     <SnakeGame />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
