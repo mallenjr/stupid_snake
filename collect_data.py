64b4f0b7fdbb890ef25084d9a9ce37a29763d988
Michael Allen <mallenjr42@gmail.com>
2021-12-04 10:25:49 -0600

    completed data collection script

---
+++ collect_data.py
@@ -0,0 +1,26 @@
+import speech_recognition as sr
+
+import constants
+
+i = 0
+command = "left"
+
+def collect_sample(r, source):
+    global i
+    print("Say something!")
+    audio = r.listen(source, phrase_time_limit=1)
+    wav_data = audio.get_wav_data()
+    file = open(f"data/{command}/out{i}.wav", "wb")
+    file.write(wav_data)
+    file.close()
+    print('speech written')
+    print(f'count = {i}')
+    i += 1
+
+if __name__ == "__main__":
+  r = sr.Recognizer()
+  r.energy_threshold = constants.mic_threshold
+  with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(duration=4, source=source)
+        while 1:
+            collect_sample(r, source)
