3761858538860e4397c457e86b8104e885e83a30
Michael Allen <mallenjr42@gmail.com>
2021-12-04 10:19:16 -0600

    cleaned listen code

---
+++ constants.py
@@ -0,0 +1,12 @@
+import speech_recognition
+import tensorflow as tf
+
+try:
+  AUTOTUNE = tf.data.AUTOTUNE     
+except:
+  AUTOTUNE = tf.data.experimental.AUTOTUNE 
+
+webserver_host_name = "0.0.0.0"
+webserver_port = 23336
+
+mic_threshold = 400
--- listen.py
+++ listen.py
@@ -1,26 +1,18 @@
 import numpy as np
 import speech_recognition as sr
-import utils
-import tensorflow as tf
 from tensorflow.keras.models import load_model
-import matplotlib.pyplot as plt
 from flask import Flask
 from flask_cors import CORS                                               
 import threading
-from random import randint
+
+import utils
+import constants
 
 model = load_model('model')
 
 commands = utils.get_commands()
 print(commands)
 
-try:
-  AUTOTUNE = tf.data.AUTOTUNE     
-except:
-  AUTOTUNE = tf.data.experimental.AUTOTUNE 
-
-host_name = "0.0.0.0"
-port = 23336
 app = Flask(__name__)
 CORS(app)
 
@@ -31,43 +23,55 @@ def main():
     global direction
     return direction
 
+
+def collect_speech(r, source):
+    print("Say something!")
+    audio = r.listen(source, phrase_time_limit=1)
+    wav_data = audio.get_wav_data()
+    file = open("data/out.wav", "wb")
+    file.write(wav_data)
+    file.close()
+    print('speech written')
+
+def infer_from_speech():
+    print('starting inference')
+    global direction
+    test = ["data/out.wav"]
+    output_ds = utils.preprocess_dataset(test)
+
+    test_audio = []
+
+    for audio, _ in output_ds:
+        test_audio.append(audio.numpy())
+
+    test_audio = np.array(test_audio)
+
+    result = model.predict(test_audio)
+    prediction = np.argmax(result, axis=1)
+    if result[0][prediction] > 0.4:
+        print(prediction[0])
+        print(commands[int(prediction[0])])
+        direction = commands[int(prediction[0])]
+    else:
+        print('no result found')
+
+
 # main method
 if __name__ == '__main__':
-    threading.Thread(target=lambda: app.run(host=host_name, port=port, debug=True, use_reloader=False)).start()
+    threading.Thread(
+        target=lambda: app.run(
+            host=constants.webserver_host_name, 
+            port=constants.webserver_port, 
+            debug=True, 
+            use_reloader=False
+        )
+    ).start()
 
     # obtain audio from the microphone
     r = sr.Recognizer()
-    r.energy_threshold = 600
+    r.energy_threshold = constants.mic_threshold
     with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(duration=4, source=source)
         while 1:
-            print("Say something!")
-            audio = r.listen(source, phrase_time_limit=1)
-            wav_data = audio.get_wav_data(convert_rate=11025)
-            decoded_audio, _ = tf.audio.decode_wav(contents=wav_data, desired_samples=16000)
-            prepared_audio = tf.squeeze(decoded_audio, axis=-1)
-            spectrogram = utils.get_spectrogram(prepared_audio)
-
-
-            file = open("data/out.wav", "wb")
-            file.write(wav_data)
-            file.close()
-
-            test = ["data/out.wav"]
-            output_ds = utils.preprocess_dataset(test)
-
-            test_audio = []
-            test_labels = []
-
-            for audio, label in output_ds:
-                test_audio.append(audio.numpy())
-                test_labels.append(label.numpy())
-
-            test_audio = np.array(test_audio)
-            test_labels = np.array(test_labels)
-
-            result = model.predict(test_audio)
-            prediction = np.argmax(result, axis=1)
-            if result[0][prediction] > 0.4:
-                print(prediction[0])
-                print(commands[int(prediction[0])])
-                direction = commands[int(prediction[0])]
+            collect_speech(r, source)
+            infer_from_speech()
