0f3b626f55cfa5326070ce1ab2ebeb3410d37aec
Michael Allen <mallenjr42@gmail.com>
2021-12-10 16:34:16 -0600

    attempting to query input devices from cpal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+use cpal::traits::{DeviceTrait, HostTrait};
+
+fn get_host() -> cpal::Host {
+    let default_host_id = &cpal::default_host().id();
+
+    let host = if cfg!(target_os = "linux") {
+        let host_id = *cpal::available_hosts()
+            .iter()
+            .find(|h| h.name()
+            .contains("jack"))
+            .unwrap_or(default_host_id);
+
+        cpal::host_from_id(host_id).unwrap()
+    } else if cfg!(target_os = "windows") {
+        let host_id = *cpal::available_hosts()
+            .iter()
+            .find(|h| h.name()
+            .contains("asio"))
+            .unwrap_or(default_host_id);
+        
+        cpal::host_from_id(host_id).unwrap()
+    } else {
+        cpal::default_host()
+    };
+
+    host
+}
+
+fn get_input_device(host: cpal::Host) -> cpal::Device {
+    let input_devices = host.input_devices().expect("Failed to query input devices");
+
+    for (pos, device) in input_devices.enumerate() {
+        let configs = device.supported_input_configs();
+
+        if let Err(_e) = configs { // We could not query inputs from this device
+            continue
+        }
+
+        for (_, config) in configs.unwrap().enumerate() {
+            println!("{:?}", config.min_sample_rate());
+        }
+
+        println!("Device {}: {}", pos, device.name().expect("Could not display device name"))
+    }
+
+    host.default_input_device().unwrap()
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let host = get_host();
+
+    println!("\nSelected Host Device: {}\n", host.id().name());
+
+    let input_device = get_input_device(host);
+
 }
